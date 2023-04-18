a5522f99512654d9b8b8fc2abede9fae1274c323
Joshua Carroll <carrolljosh1976@gmail.com>
2023-04-17 22:41:11 -0400

    added h1 and input for Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+    {/*Left Side*/}
+        <div>
+            <h1>Josh's Games</h1>
+        </div>
+        <div>
+            <input type="text" placeholder='Search games...' />
+        </div>
+    </div>
+  )
+}
