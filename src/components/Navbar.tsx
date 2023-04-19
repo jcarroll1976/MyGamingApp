1ca565165f01ed794b560b36ead54e26701a314c
Joshua Carroll <carrolljosh1976@gmail.com>
2023-04-18 20:37:52 -0400

    Added background color, login and signup links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,16 @@ export default function Navbar() {
         <div>
             <h1>Josh's Games</h1>
         </div>
+    {/*Middle*/}
         <div>
             <input type="text" placeholder='Search games...' />
+        </div>
+    {/*Right Side*/}
+        <div>
+            <ul className='navbarList'>
+                <li className='navbarlistItem'>Login</li>
+                <li className='navbarlistItem'>Sign Up</li>
+            </ul>
         </div>
     </div>
   )
