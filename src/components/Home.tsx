281e76203a91cec72e97652e01119495448f54b4
Joshua Carroll <carrolljosh1976@gmail.com>
2023-11-05 20:43:57 -0500

    started to add styles for homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import React,{useEffect,useState} from 'react';
 import { Results } from '../models/GameResponse';
 import { fetchGames} from '../services/GameApiService';
 import {Link} from "react-router-dom";
+import "./Home.css"
 
 function Home() {
     const [results,setResults] = useState<Results[]>([]);
@@ -13,12 +14,19 @@ function Home() {
     })
   },[])
   return (
-    <div>
-        <h2>Top 25 Latest Releases of {currentYear}</h2>
-        <ul className='results-list'>
-          {results.map((result,i) =>
-          <li key={i}><h3><Link to={`/game/${result.slug}`}>{result.name}</Link></h3><div className='gameImage-div'><img className='game-image' src={result.background_image} alt=''/></div></li>)}
-        </ul>
+    <div className='home-div'>
+        <div>
+            <h2>Top 25 Latest Releases of {currentYear}</h2>
+        </div>
+        <div>
+            <ul className='results-list'>
+                {results.map((result,i) =>
+                <li key={i}>
+                    <h3><Link to={`/game/${result.slug}`}>{result.name}</Link></h3>
+                    <div className='gameImage-div'><img className='game-image' src={result.background_image} alt=''/></div>
+                </li>)}
+            </ul>
+        </div>
     </div>
   )
 }
